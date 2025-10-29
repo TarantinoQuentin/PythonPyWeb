999984e2e6f6f211e88d4175a8417389a97c48c1
gridan <gregdenfv42@gmail.com>
2025-10-29 21:08:56 +0300

    Пятая лабораторная в процессе на занятии

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, filters
+from rest_framework import status, filters, permissions, authentication
 from django.views.decorators.csrf import csrf_exempt  # Чтобы post, put, patch, delete не требовали csrf токена (небезопасно)
 from apps.db_train_alternative.models import Author
 from .serializers import AuthorSerializer, AuthorModelSerializer
@@ -12,6 +12,29 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework_simplejwt.authentication import JWTAuthentication
+# 522ef8211c4e1f331aa6a07f80948743fc31f233
+
+class CustomPermission(permissions.BasePermission):
+    """
+    Пользователи могут выполнять различные действия в зависимости от их роли.
+    """
+
+    def has_permission(self, request, view):
+        # Разрешаем только GET запросы для неаутентифицированных пользователей
+        if request.method == 'GET' and not request.user.is_authenticated:
+            return True
+
+        # Разрешаем GET и POST запросы для аутентифицированных пользователей
+        if request.method in ['GET', 'POST'] and request.user.is_authenticated:
+            return True
+
+        # Разрешаем все действия для администраторов
+        if request.user.is_superuser:
+            return True
+
+        # Во всех остальных случаях возвращаем False
+        return False
 
 
 class AuthorPagination(PageNumberPagination):
@@ -48,6 +71,11 @@ class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, C
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
 
+    # Переопределяем атрибут permission_classes для указания нашего собственного разрешения
+    permission_classes = [CustomPermission]
+    # authentication_classes = [authentication.TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
+
     def get(self, request, *args, **kwargs):
         if kwargs.get(self.lookup_field):  # если был передан id или pk
             try:
@@ -73,6 +101,8 @@ class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, C
 
 
 class AuthorAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     @csrf_exempt
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
